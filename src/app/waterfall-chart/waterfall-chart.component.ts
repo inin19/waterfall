9be0f86f4e421e290ca8b44ceb50905752abb3d3
U1118608 <ying-ying.chen@marsh.com>
2018-03-21 10:04:59 -0400

    add all dependency
    add all dependency

--- src/app/waterfall-chart/waterfall-chart.component.ts
+++ src/app/waterfall-chart/waterfall-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import * as crossfilter from 'crossfilter2';
 
 @Component({
   selector: 'app-waterfall-chart',
@@ -9,7 +11,13 @@ export class WaterfallChartComponent implements OnInit {
 
   constructor() { }
 
+  abc = [1, 2, 3];
+  ndx: any;
+
   ngOnInit() {
+    this.ndx = crossfilter(this.abc);
+
+
   }
 
 }
